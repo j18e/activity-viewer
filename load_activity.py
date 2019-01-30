caf83e58b26e80bf18768e982971f7beb0e6e6e4
Jamie Wiebe <@jamesmacfarlane>
2019-01-30 14:07:09 +0100

    retreive content from browser history

--- load_activity.py
+++ load_activity.py
@@ -3,32 +3,44 @@
 from os import environ
 from re import match
 from influxdb import InfluxDBClient
-from json import dumps
+import sqlite3
 
 def post_data(client, data, measurement_name):
+    print(type(data))
+    print(len(data))
     body = [{"measurement": measurement_name, "time": m[0],
              "fields": {"value": m[1]}} for m in data]
     client.write_points(body)
 
+def load_browser_history(browser_db_file):
+    conn = sqlite3.connect(browser_db_file)
+    cur = conn.cursor()
+    cur.execute("SELECT id, url FROM moz_places")
+    places = {p[0]: p[1] for p in cur.fetchall()}
+    cur.execute("SELECT visit_date, place_id FROM moz_historyvisits")
+    history = [(e[0] * 1000, places[e[1]]) for e in cur.fetchall()]
+    return history
 
-browser_history_file = environ["BROWSER_TIMESTAMP_CSV"]
+def load_zsh_history(zsh_history_file):
+    with open(zsh_history_file, 'r') as s:
+        zsh_history = [l for l in s.readlines()]
+    zsh_history = [
+        (int(l.split(':')[1].strip()) * 1000000000, l.split(':0;')[1].strip())
+        for l in zsh_history if match(r'^: \d{10,15}:0;', l)
+    ]
+    return zsh_history
+
+browser_db_file = environ["BROWSER_DB_FILE"]
 zsh_history_file = environ["ZSH_HISTORY_FILE"]
 influxdb_host = environ["INFLUXDB_HOST"]
 
-with open(browser_history_file, 'r') as s:
-    browser_timestamps = [(int(l.strip()) * 1000, 1) for l in s.readlines()]
-browser_timestamps.sort()
-
-with open(zsh_history_file, 'r') as s:
-    zsh_timestamps = [(int(l.split(':')[1].strip()) * 1000000000,
-                       l.split(':0;')[1].strip())
-                      for l in s.readlines() if match(r'^: \d{10,15}:0;', l)]
-zsh_timestamps.sort()
+browser_history = load_browser_history(browser_db_file)
+zsh_history = load_zsh_history(zsh_history_file)
 
 database_name = "activity"
 client = InfluxDBClient(host=influxdb_host, database=database_name)
 client.create_database(database_name)
 
-post_data(client, zsh_timestamps, "zsh_activity")
-post_data(client, browser_timestamps, "browser_activity")
+post_data(client, zsh_history, "zsh_activity")
+post_data(client, browser_history, "browser_activity")
 
