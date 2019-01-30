d82423b8313a70f22196963177a29a10f9aadefd
Jamie Wiebe <@jamesmacfarlane>
2019-01-30 12:55:37 +0100

    retreive content from zsh history

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,6 @@ FROM alpine:3.8
 RUN apk add --update \
     sqlite \
     python3 \
-    py3-requests
+    py3-pip
+
+RUN pip3 install --upgrade influxdb requests
--- load_activity.py
+++ load_activity.py
@@ -1,41 +1,34 @@
 #!/usr/bin/env python3
 
 from os import environ
-from requests import post
 from re import match
+from influxdb import InfluxDBClient
+from json import dumps
 
-def write_data(payload):
-    path = "/write?db=activity"
-    resp = post(influxdb_server + path, data=payload)
-    resp.raise_for_status()
+def post_data(client, data, measurement_name):
+    body = [{"measurement": measurement_name, "time": m[0],
+             "fields": {"value": m[1]}} for m in data]
+    client.write_points(body)
 
-def compile_timestamps(timestamps, db_name):
-    count = 0
-    payload = ""
-    for ts in timestamps:
-        payload += "{} value=1 {}\n".format(db_name, str(ts))
-        count+=1
-        if count > 99:
-            write_data(payload)
-            payload = ""
-            count = 0
 
 browser_history_file = environ["BROWSER_TIMESTAMP_CSV"]
 zsh_history_file = environ["ZSH_HISTORY_FILE"]
-influxdb_server = environ["INFLUXDB_SERVER"]
+influxdb_host = environ["INFLUXDB_HOST"]
 
 with open(browser_history_file, 'r') as s:
-    browser_timestamps = [int(l.strip()) * 1000 for l in s.readlines()]
+    browser_timestamps = [(int(l.strip()) * 1000, 1) for l in s.readlines()]
 browser_timestamps.sort()
 
 with open(zsh_history_file, 'r') as s:
-    zsh_timestamps = [int(l.split(':')[1].strip()) * 1000000000
+    zsh_timestamps = [(int(l.split(':')[1].strip()) * 1000000000,
+                       l.split(':0;')[1].strip())
                       for l in s.readlines() if match(r'^: \d{10,15}:0;', l)]
 zsh_timestamps.sort()
 
-resp = post(influxdb_server + "/query", params={"q": "CREATE DATABASE activity"})
-resp.raise_for_status()
+database_name = "activity"
+client = InfluxDBClient(host=influxdb_host, database=database_name)
+client.create_database(database_name)
 
-compile_timestamps(browser_timestamps, "browser_activity")
-compile_timestamps(zsh_timestamps, "zsh_activity")
+post_data(client, zsh_timestamps, "zsh_activity")
+post_data(client, browser_timestamps, "browser_activity")
 
